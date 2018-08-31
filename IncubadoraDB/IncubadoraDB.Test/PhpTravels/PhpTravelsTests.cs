6f486ed06e3449d1127551bbb7e9714ea8984d1c
thiagol <thiago.m.lima@outlook.com>
2018-08-31 13:30:18 -0300

    Scaffolding

---
+++ IncubadoraDB/IncubadoraDB.Test/PhpTravels/PhpTravelsTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Data;
+
+namespace IncubadoraDB.Test.PhpTravels
+{
+    [TestClass]
+    public class PhpTravelsTests : TestBase
+    {
+        public PhpTravelsTests() { }
+
+        [TestMethod]
+        [DataSource("Microsoft.VisualStudio.TestTools.DataSource.CSV", "Csv\\TC_001.csv", "TC_001#csv", DataAccessMethod.Sequential)]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
